4e2efab8e6526a1a7d78c51cd80c333f3f65034c
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-10 08:54:51 +0530

    create authContext

---
+++ client/src/context/Authcontext.jsx
@@ -0,0 +1,46 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const storedData = JSON.parse(localStorage.getItem("user_data"));
+
+  useEffect(() => {
+    if (storedData) {
+      const { userToken, user } = storedData;
+      setToken(userToken);
+      setUserData(user);
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const login = (newToken, newData) => {
+    localStorage.setItem(
+      "user_data",
+      JSON.stringify({ userToken: newToken, user: newData })
+    );
+    setToken(newToken);
+    setUserData(newData);
+    setIsAuthenticated(true);
+  };
+
+  const logOut = () => {
+    localStorage.removeItem("user_data");
+    setIsAuthenticated(false);
+    setToken(null);
+    setUserData(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, token, login, logOut, userData }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);

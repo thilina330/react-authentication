22744b43ed6bbc067ad2ca26e8246842bacf3a3b
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-10 08:54:05 +0530

    create sign up page

--- client/src/auth/Register.jsx
+++ client/src/auth/Register.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
-import {Card, Form, Typography,Flex, Input} from 'antd'
+import {Card, Form, Typography,Flex, Input, Button, Alert} from 'antd'
+import { Link } from 'react-router-dom';
+import registerImage from '../assets/OIP.jpg'
+import useSignUp from '../hooks/useSignUp';
 
 
 const Register = () => {
 
+  const {loading,error,registerUser} = useSignUp()
+
   const handleRegister = (values) => {
-     console.log(values);
+     registerUser(values)
   }
 
   return (
    <Card className='form-contanier'>
-     <Flex>
+     <Flex gap="large">
       {/* form create */}
        <Flex vertical flex={1}>
               <Typography.Title level={3} strong className='title'>Create an account</Typography.Title>
@@ -23,14 +28,68 @@ const Register = () => {
                     message:"please input your full name!"
 
                   }]}>
-                  <Input placeholder='Enter your full name'/>
-                </Form.Item>
+                        <Input size='large' placeholder='Enter your full Name'/>
+                  </Form.Item>
+
+                  <Form.Item label="Email" name="email" rules={[{
+
+                  required:true,
+                  message:"please input your Email!"
+
+                  },{
+                    type: "email",
+                    message: "input valid email!"
+                  }
+                  ]}>
+                      <Input size='large' placeholder='Enter your Email'/>
+                  </Form.Item>
+
+                  <Form.Item label="Password" name="password" rules={[{
+
+                    required:true,
+                    message:"please input your password!"
+
+                    }
+                    ]}>
+                        <Input.Password size='large' placeholder='Enter your Password!'/>
+                  </Form.Item>
+
+                  <Form.Item label="Confirm-Password" name="confirmpassword" rules={[{
+
+                    required:true,
+                    message:"please input Re-Password!"
+
+                    }
+                    ]}>
+                        <Input.Password size='large' placeholder='Re-Enter your Password!'/>
+                  </Form.Item>
+                  {
+                    error && <Alert description={error} type='error' showIcon closable className='alert' />
+                  }
+                  <Form.Item >
+                    <Button 
+                    type={`${loading ? '' : ''}`} 
+                    size="large" htmlType="submit" 
+                    className='btn'
+                  
+                    >
+                     {loading? <Span/> : 'Create Account'}
+                   
+                    </Button>
+                  </Form.Item>
+
+                  <Form.Item>
+                    
+                    <Link to="/login">
+                      <Button size='large' className='btn'>Sign In</Button>
+                    </Link>
+                  </Form.Item>
             
               </Form>
        </Flex>
        {/* image create */}
-       <Flex>
-
+       <Flex flex={1}>
+          <img src={registerImage} className='auth-image'/>
        </Flex>
      </Flex>
    </Card>

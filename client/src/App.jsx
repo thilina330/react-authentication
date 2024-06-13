584cffe259c87df81988fc4304f3c32984feaf5b
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-13 13:13:31 +0530

    create routing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
-import Register from './auth/Register'
-import Signin from './auth/Signin'
-import Dashbord from './pages/Dashbord'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Register from './auth/Register';
+import Signin from './auth/Signin';
+import Dashboard from './pages/Dashbord';
+import { useAuth } from './context/AuthContext.jsx'; // Ensure AuthProvider is imported
 import './App.css'
 
-
 const App = () => {
+  const { isAuthenticated } = useAuth(); // Hook to check authentication
+
   return (
+    
+      <Router>
+        <Routes>
+          <Route path='/' element={!isAuthenticated ? <Register /> : <Navigate to='/dashboard' />} />
+          <Route path='/login' element={!isAuthenticated ? <Signin /> : <Navigate to='/dashboard' />} />
+          <Route path='/dashboard' element={isAuthenticated ? <Dashboard /> : <Signin/>} />
+          {/* <Route path='/signup' element={<Register />} /> */}
+        </Routes>
+      </Router>
    
-    <Router>
-      <Routes>
-         <Route path='/' element={<Register/>}></Route>
-         <Route path='/login' element={<Signin/>}></Route>
-         <Route path='/dashbord' element={<Dashbord/>}></Route>
-      </Routes>
-    </Router>
-      
- 
-  )
+  );
 }
 
-export default App
+export default App;

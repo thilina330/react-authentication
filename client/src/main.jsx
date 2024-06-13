fe21a0d2097b01443cbc034a2d92defd5fdbf0ce
Thilina Lakshan <126488588+thilina330@users.noreply.github.com>
2024-06-13 13:16:30 +0530

    create main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { AuthProvider } from './context/Authcontext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
